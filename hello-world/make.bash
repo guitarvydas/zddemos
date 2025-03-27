dd3af53dfe4b2ad836144dc92a78b99f9c494400
Paul Tarvydas <paul.tarvydas@gmail.com>
2025-03-27 08:26:02 -0400

    arithcl

--- hello-world/make.bash
+++ hello-world/make.bash
@@ -1,9 +1,9 @@
 #!/bin/bash
 node ../zd/das2json.mjs <hello-world.drawio >hello-world.json
-python3 main.py . - '' nondeterministic hello-world.json | jq --compact-output .
-python3 main.py . - '' seq1 hello-world.json | jq --compact-output .
-python3 main.py . - '' seq2 hello-world.json | jq --compact-output .
-python3 main.py . - '' seq3 hello-world.json | jq --compact-output .
-python3 main.py . - '' seq3a hello-world.json | jq --compact-output .
-python3 main.py . - '' seq4 hello-world.json | jq --compact-output .
-python3 main.py . - '' seq4a hello-world.json | jq --compact-output .
+python3 main.py . - '' nondeterministic hello-world.json | jq -M --compact-output .
+python3 main.py . - '' seq1 hello-world.json | jq -M --compact-output .
+python3 main.py . - '' seq2 hello-world.json | jq -M --compact-output .
+python3 main.py . - '' seq3 hello-world.json | jq -M --compact-output .
+python3 main.py . - '' seq3a hello-world.json | jq -M --compact-output .
+python3 main.py . - '' seq4 hello-world.json | jq -M --compact-output .
+python3 main.py . - '' seq4a hello-world.json | jq -M --compact-output .
--- make.bash
+++ make.bash
@@ -1,13 +1,6 @@
 #!/bin/bash
 cd hello-world
-node ../zd/das2json.mjs <hello-world.drawio >hello-world.json
-python3 main.py . - '' nondeterministic hello-world.json
-python3 main.py . - '' seq1 hello-world.json
-python3 main.py . - '' seq2 hello-world.json
-python3 main.py . - '' seq3 hello-world.json
-python3 main.py . - '' seq3a hello-world.json
-python3 main.py . - '' seq4 hello-world.json
-python3 main.py . - '' seq4a hello-world.json
+./make.bash
 cd ..
 
 cd arith
