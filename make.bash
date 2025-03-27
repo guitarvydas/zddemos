1cf8ed002eb17bd459a5aa762b1ea1a704ef2b08
Paul Tarvydas <paul.tarvydas@gmail.com>
2025-03-26 23:18:38 -0400

    hello world  indeterminate, seq1 and seq2

--- make.bash
+++ make.bash
@@ -1,5 +1,7 @@
 #!/bin/bash
 cd hello-world
 node ../zd/das2json.mjs <hello-world.drawio >hello-world.json
-python3 main.py . - '' main hello-world.json
+python3 main.py . - '' indeterminate hello-world.json
+python3 main.py . - '' seq1 hello-world.json
+python3 main.py . - '' seq2 hello-world.json
 cd ..
