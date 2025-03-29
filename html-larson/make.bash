e07e9e70a634a2b947f69afcea4a84d69278b282
Paul Tarvydas <paul.tarvydas@gmail.com>
2025-03-28 22:14:13 -0400

    edit echo messages in make.bash

--- html-larson/make.bash
+++ html-larson/make.bash
@@ -1,3 +1,6 @@
 #!/bin/bash
+echo ' the Info messages are progress indicators'
 node ../zd/das2json.mjs <code-generator.drawio >code-generator.json
 python3 main.py . - '' main code-generator.json | node ../util/decodeoutput.js
+echo 'open a browser on ./ledst.html'
+echo 'the leds should cycle from left to right then back right to left for a while, then stop'
